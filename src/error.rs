6945acb094d68b3bee69bab9df97506cd5eceb73
Douglas Mendes <mendes-barreto@live.com>
2021-10-16 13:46:24 -0400

    Add method to unpack the amount of token to be used by the initializer

---
+++ src/error.rs
@@ -0,0 +1,16 @@
+use thiserror::Error;
+
+use solana_program::program_error::ProgramError;
+
+#[derive(Error, Debug, Copy, Clone)]
+pub enum EscrowError {
+    /// Invalid instruction
+    #[error("Invalid Instruction")]
+    InvalidInstruction,
+}
+
+impl From<EscrowError> for ProgramError {
+    fn from(err: EscrowError) -> ProgramError {
+        ProgramError::Custom(err as u32)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
 pub mod entrypoint;
+mod error;
